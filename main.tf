40bd92bdebdcefe006adddc2346fc0d5da2485cb
HarshaVardhan525 <harshavardhangangumolu@gmail.com>
2026-07-03 16:22:03 +0530

    Created Auto Scaling Group and Load balancer To launch Mutiple servers for web application

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.53.0"
+    }
+  }
+}
+
+
+
 provider "aws" {
     region = "ap-south-1"
 }    
@@ -17,6 +28,16 @@ resource "aws_subnet" "my_subnet" {
     tags = {
         Name = "my_subnet"
     }
+}
+resource "aws_subnet" "my_subnet2" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "ap-south-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "my_subnet2"
+  }
 }
 
 resource "aws_internet_gateway" "my_igw" {
@@ -41,16 +62,20 @@ resource "aws_route_table_association" "my_route_table_association" {
     subnet_id = aws_subnet.my_subnet.id
     route_table_id = aws_route_table.my_route_table.id
 }
+resource "aws_route_table_association" "my_route_table_association2" {
+  subnet_id      = aws_subnet.my_subnet2.id
+  route_table_id = aws_route_table.my_route_table.id
+}
 
 variable "server_port" {
     description = "Port used for http requests"
     type = number
-    default = 8080
+    default = 80
 }
 variable "ssh_port" {
     description = "Port used for http requests"
     type = number
-    default = 8080
+    default = 22
 }
 
 resource "aws_security_group" "my_security_group" {
@@ -79,22 +104,134 @@ resource "aws_security_group" "my_security_group" {
     }
 }
 
-resource "aws_instance" "my_ec2" {
-    ami = "ami-0d351f1b760a30161"
-    instance_type = "t3.micro"
-    key_name = "Mumbai-Key"
-    subnet_id = aws_subnet.my_subnet.id
-    vpc_security_group_ids = [aws_security_group.my_security_group.id]
-  
+resource "aws_launch_template" "my_lt" {
+  name_prefix   = "my-launch-template-"
+  image_id      = "ami-0d351f1b760a30161"
+  instance_type = "t3.micro"
+  key_name      = "Mumbai-Key"
+
+  vpc_security_group_ids = [
+    aws_security_group.my_security_group.id
+  ]
+
+user_data = base64encode(<<-EOF
+#!/bin/bash
+dnf update -y
+dnf install -y nginx
+
+systemctl enable nginx
+systemctl start nginx
+
+echo "<h1>Welcome to my EC2 instance</h1>" > /usr/share/nginx/html/index.html
+EOF
+)
+  tag_specifications {
+    resource_type = "instance"
 
     tags = {
-        Name = "my_ec2"
+      Name = "my_ec2"
     }
-
+  }
 }
 
-output "public_ip" {
-    description = "The Public ip of the ec2 instance"
-    value = aws_instance.my_ec2.public_ip
-    
+
+
+resource "aws_autoscaling_group" "my_asg" {
+ launch_template {
+  id      = aws_launch_template.my_lt.id
+  version = "$Latest"
+}
+
+  min_size         = 2
+  max_size         = 4
+  desired_capacity = 2
+
+  vpc_zone_identifier = [
+    aws_subnet.my_subnet.id,
+    aws_subnet.my_subnet2.id
+  ]
+
+  target_group_arns = [aws_lb_target_group.my_target_group.arn]
+  health_check_type = "ELB"
+
+  tag {
+    key                 = "Name"
+    value               = "my_asg"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_lb" "my_lb" {
+    name               = "my-asg"
+    internal           = false
+    load_balancer_type = "application"
+    security_groups    = [aws_security_group.alb.id]
+    subnets = [
+        aws_subnet.my_subnet.id,
+        aws_subnet.my_subnet2.id
+        ]
+
+    tags = {
+        Name = "my_lb"
+    }
+}
+resource "aws_lb_listener" "http_listener" {
+    load_balancer_arn = aws_lb.my_lb.arn
+    port              = var.server_port
+    protocol          = "HTTP"
+
+    default_action {
+        type             = "forward"
+        target_group_arn = aws_lb_target_group.my_target_group.arn
+    }
+}
+resource "aws_security_group" "alb" {
+  name        = "alb-security-group"
+  description = "Security group for Application Load Balancer"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb-security-group"
+  }
+}
+
+resource "aws_lb_target_group" "my_target_group" {
+    name     = "my-target-group"
+    port     = var.server_port
+    protocol = "HTTP"
+    vpc_id = aws_vpc.my_vpc.id
+
+    health_check {
+        path                = "/"
+        protocol            = "HTTP"
+        interval            = 15
+        timeout             = 5
+        healthy_threshold   = 2
+        unhealthy_threshold = 2
+        matcher             = "200"
+    }
+
+    tags = {
+        Name = "my_target_group"
+    }
+}
+
+
+output "alb_dns_name" {
+    description = "The DNS name of the Application Load Balancer"
+    value = aws_lb.my_lb.dns_name
 }
